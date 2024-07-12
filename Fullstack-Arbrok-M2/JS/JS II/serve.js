a2849892b803cccb93d3b88e779df485707980f7
Nahuelsan <nahuelsan96@gmail.com>
2024-07-12 11:03:11 -0300

    add function in a server node

--- Fullstack-Arbrok-M2/JS/JS II/serve.js
+++ Fullstack-Arbrok-M2/JS/JS II/serve.js
@@ -1,26 +1,53 @@
 const express = require("express");
 const app = express();
 /**
- * 
- * @param {*} user 
+ *
+ * @param {*} user
  */
-function register(user) {
-    /**
-    *   use : {
-    *       email: "test@gmail.com"
-    *       passwword: "123456"
-    *       name: "Nahuel"    
-    * }
-    */
-    // Buscar un usuario en la DB con ese email
-    // return "ese usuario ya esta registrado"
-    // Crifrar la contraseña
-    // Crear el usuario en la BD 
-    // return send.status(200)
-}
 
 app.listen(5000, () => {
-  showCount(0);
-  // 0
-  showCount(null);
+  console.log("Server running");
 });
+
+/* BD */
+const users = [];
+
+function register(user) {
+  // Buscar un usuario en la DB con ese email
+  // return "ese usuario ya esta registrado"
+  // Crifrar la contraseña
+  // Crear el usuario en la BD
+  // return send.status(200)
+
+  users.push(user)
+}
+
+/* 
+  request === req (Petición que hace el usuario) 
+  response === res (Respuesta que le vamos a dar al usuario)
+*/
+
+app.get("/", (req, res) => {
+  return res.send(users);
+});
+
+app.get("/register", (req, res) => {
+  register({ name: "Nahuel", email: "nahuelsan96@gmail.com" });
+  return res.send("Usuario agregado");
+});
+
+
+
+/* Funciones con parametros */
+
+/* Funcion push de array */
+
+/* VERBOS */
+
+// GET
+// POST 
+// PUT 
+// DELETE
+// PACTH 
+
+
